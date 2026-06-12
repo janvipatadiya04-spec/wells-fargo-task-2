498424ba70d07e4bd9610f5f89ea7c4ccd1cf250
janvipatadiya04-spec <janvipatadiya04@gmail.com>
2026-06-12 17:13:59 +0530

    Add files via upload

---
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -0,0 +1,76 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue()
+    private long clientId;
+
+    @ManyToOne
+    @JoinColumn(name = "advisor_id", nullable = false)
+    private Advisor advisor;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    protected Client() {
+
+    }
+
+    public Client(Advisor advisor, String firstName, String lastName, String email) {
+        this.advisor = advisor;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public Advisor getAdvisor() {
+        return advisor;
+    }
+
+    public void setAdvisor(Advisor advisor) {
+        this.advisor = advisor;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,65 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import java.time.LocalDate;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @OneToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private LocalDate createdAt;
+
+    protected Portfolio() {
+
+    }
+
+    public Portfolio(Client client, String name, LocalDate createdAt) {
+        this.client = client;
+        this.name = name;
+        this.createdAt = createdAt;
+    }
+
+    public Long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+}
